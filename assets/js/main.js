4d5ab602307c081c366e2d54321f70357f1dcfcf
Josué Juca <josuejuca19@gmail.com>
2022-10-22 16:33:10 -0300

    [ 22/10/2022 ] Atualização 💙
    Modificações:
    - 404.html
    - index.html
    - style.css
    - main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,4 @@
 import { initScrollReveal } from "./scrollReveal.js";
-import { hoverChangeExperience } from "./hoverChangeExperience.js";
 import { typeWrite } from "./typeWrite.js";
 import { hoverChangeDescription } from "./hoverChangeDescription.js";
 import { menu } from "./menu.js";
@@ -7,48 +6,6 @@ import { menu } from "./menu.js";
 menu();
 initScrollReveal();
 typeWrite(document.querySelector(".typewriter"));
-
-hoverChangeExperience(
-  ".digitalhouse",
-  `Atualmente trabalho como professor conteudista em frontend na
-  Digital House, o trabalho consiste em criar conteúdos para as
-  aulas de especialização frontend. Alguns temas dos conteúdos
-  realizados foram: React, Redux, TypeScript, Testes,
-  GraphQL, Next.js, MUI, React Hook Form e styled-components.`,
-  "Professor conteudista em Frontend",
-  "Digital House",
-  "Nov 2021 - Atual"
-);
-
-hoverChangeExperience(
-  ".zuplae",
-  `Trabalhei como Social Media e Designer na Zuplae que é um escola
-  de programação, onde teve como foco as criações de conteúdos sobre
-  programação para a comunidade dev através das redes sociais.`,
-  "Social Media e UI Designer",
-  "Zuplae",
-  "Jan 2022 - Abril 2022"
-);
-
-hoverChangeExperience(
-  ".codigofontetv",
-  `Trabalhei como Social Media e Designer no Código Fonte TV, 
-  onde teve como foco as criações de conteúdos sobre programação 
-  para a comunidade dev através das redes sociais do CDF.`,
-  "Social Media e Designer",
-  "Código Fonte TV",
-  "Jun 2021 - Jan 2022"
-);
-
-hoverChangeExperience(
-  ".contweb",
-  `Realizei o estágio na ContWeb, uma empresa de contabilidade. 
-  Onde pela primeira vez tive experiência com o React. Além do
-  desenvolvimento frontend criei os designs da nova plataforma da empresa.`,
-  "Developer frontend e UI Designer",
-  "ContWeb",
-  "Set 2021 - Nov 2021"
-);
 
 hoverChangeDescription(
   ".html",
@@ -71,14 +28,11 @@ hoverChangeDescription(
   "ReactJS é uma biblioteca JavaScript de código aberto com foco em criar interfaces de usuário em páginas web."
 );
 hoverChangeDescription(
-  ".next",
-  "Next.js é uma estrutura da web que permite funcionalidades como renderização do lado do servidor e geração de sites estáticos para web baseados em React."
+  ".c",
+  "C é uma linguagem de programação compilada de propósito geral."
 );
 hoverChangeDescription(
-  ".styled",
-  "Styled Components é uma biblioteca que utiliza o conceito de CSS-in-JS, ou seja, que nos permite escrever códigos CSS dentro do Javascript."
+  ".python",
+  "Python é uma linguagem de programação de alto nível, dinâmica, interpretada, modular, multiplataforma e orientada a objetos."
 );
-hoverChangeDescription(
-  ".tailwind",
-  "Tailwind CSS é um framework desenvolvido para maximizar o potencial do bom e velho CSS e levá-lo ainda mais longe."
-);
+
